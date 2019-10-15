f0e89e41979f722487886e1cde10945dc87c1e96
mmFX <maxrioseco@gmail.com>
2019-10-14 23:03:14 -0300

    PogoPC final
    Was useful for Stevens HydraProbe RS232 -> SDI12 when the datalogger
    included (old PocketPC) fails (mainly because connector to the PDA dies
    under field usage). So, a simple netbook with a simple Serial USB
    converter does the job, with this simple tool.

---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PogoPC
+{
+    public partial class Form1 : Form
+    {
+        bool connected = false;
+        PogoSerial pogoSerial;
+
+        public string[] T0Data;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pogoSerial = new PogoSerial(this);
+            comboBox1.DataSource = pogoSerial.GetPorts();
+            //disconnectPogo();
+            //pogoSerial.SoilTypeChanged += new EventHandler(pogoSerial_SoilTypeChanged);
+        }
+
+
+        public void ClearSoilTYpe()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(UpdateSoilType));
+            }
+            else
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                radioButton4.Checked = false;
+
+            }
+        }
+        
+        public void UpdateSoilType()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(UpdateSoilType));
+            }
+            else
+            {
+                switch (pogoSerial.GetSoilType())
+                {
+                    case 1:
+                        radioButton1.Select();
+                        break;
+                    case 2:
+                        radioButton2.Select();
+                        break;
+                    case 3:
+                        radioButton3.Select();
+                        break;
+                    case 4:
+                        radioButton4.Select();
+                        break;
+                }
+            }
+        }
+
+        void connectPogo()
+        {
+            // pogo serial connect
+            if (pogoSerial.PogoConnect(comboBox1.Text))
+            {
+                connected = true;
+                button1.Image = Properties.Resources.conectado;
+                button1.Text = "Desconectar";
+                comboBox1.Enabled = false;
+                groupBox4.Enabled = true;
+                groupBox1.Enabled = true;
+                groupBox2.Enabled = true;
+                groupBox3.Enabled = true;
+                button2.Enabled = true;
+                label2.Text = "-.-";
+                label3.Text = "-.-";
+                label4.Text = "-.-";
+            }
+        }
+
+        void disconnectPogo()
+        {
+            // pogo serial disconnect
+            if (pogoSerial.PogoDisconnect())
+            {
+                connected = false;
+                button1.Image = Properties.Resources.desconectado;
+                button1.Text = "Conectar";
+                comboBox1.Enabled = true;
+                groupBox4.Enabled = false;
+                groupBox1.Enabled = false;
+                groupBox2.Enabled = false;
+                groupBox3.Enabled = false;
+                button2.Enabled = false;
+                ClearSoilTYpe();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                disconnectPogo();
+            }
+            else
+            {
+                connectPogo();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "-.-";
+            label3.Text = "-.-";
+            label4.Text = "-.-";
+            
+            pogoSerial.Measure();
+
+        }
+
+        internal void UpdateWithT0()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(UpdateWithT0));
+            }
+            else
+            {
+                label2.Text = (Decimal.Parse(T0Data[0]) * 100).ToString("F2");
+                label3.Text = (Decimal.Parse(T0Data[1]) * 10).ToString("F2");
+                label4.Text = Decimal.Parse(T0Data[2]).ToString();
+
+            }
+            
+        }
+    }
+}
---
+++ PogoSerial.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PogoPC
+{
+    class PogoSerial
+    {
+
+        System.IO.Ports.SerialPort port;
+        string buffer;
+        string lastCommand;
+        int soilType;
+        Form1 form;
+
+        public PogoSerial(Form1 form)
+        {
+            this.form = form;
+
+            buffer = "";
+            
+        }
+
+        public event EventHandler SoilTypeChanged;
+
+        public string[] GetPorts()
+        {
+            return System.IO.Ports.SerialPort.GetPortNames();
+        }
+
+        public bool PogoConnect(string portName)
+        {
+            port = new System.IO.Ports.SerialPort(portName, 9600, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
+            port.ReceivedBytesThreshold = 1;
+            port.ReadTimeout = 1000;
+            port.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(port_DataReceived);
+            port.Open();
+            //System.Threading.Thread.Sleep(2000);
+            if (port.IsOpen)
+            {
+                // inicializar pogo
+                //System.Threading.Thread.Sleep(2000);
+                lastCommand = "ST";
+                PogoSend("0");
+                PogoSend("0");
+                PogoSend("0");
+                PogoSend("S");
+                PogoSend("T");
+                PogoSend("=");
+                PogoSend("?");
+                PogoSend("\r");
+                PogoSend("\n");
+                //System.Threading.Thread.Sleep(2000);
+ 
+                return true;
+            }
+
+            return false;
+        }
+
+        void port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if (e.EventType == System.IO.Ports.SerialData.Chars)
+            {
+                buffer = ((System.IO.Ports.SerialPort)sender).ReadLine();
+                processResponse();
+                /*
+                string c = port.ReadExisting();
+                //Console.Out.Write(Convert.ToChar(c));
+
+                if (c == "\r")
+                {
+                }
+
+                else if (c == "\n")
+                {
+                    // process response
+                    processResponse();
+                    //Console.Out.WriteLine(buffer);
+                    //buffer = new StringBuilder();
+                }
+                else
+                {
+                    buffer.Append(c); 
+                }*/
+
+                
+            }
+
+        }
+
+        public bool PogoDisconnect()
+        {
+            port.Close();
+            if (!port.IsOpen) {
+                return true;
+            }
+            return false;
+        }
+
+        public void PogoSend(string c)
+        {
+            port.Write(c);
+            System.Threading.Thread.Sleep(10);
+        }
+
+
+
+        private void processResponse()
+        {
+            if (lastCommand.Equals("ST")) {
+                Console.WriteLine("Response: " + buffer.ToString());
+                lastCommand = "";
+                setSoilType(Int16.Parse(buffer.ToString()));
+                form.UpdateSoilType();
+                buffer = "";
+                return;
+            }
+
+            if (lastCommand.Equals("T0"))
+            {
+                Console.WriteLine("Response: " + buffer.ToString());
+                string d = buffer.ToString().Substring(3);
+                string[] data = d.Split(',');
+
+                lastCommand = "";
+                form.T0Data = data;
+                form.UpdateWithT0();
+
+                /*setSoilType(Int16.Parse(buffer.ToString()));
+                form.UpdateSoilType();*/
+                buffer = "";
+                return;
+            }
+
+        }
+
+        private void setSoilType(int soilType) {
+            this.soilType = soilType;
+            //SoilTypeChanged(this, new EventArgs());
+        }
+
+        public int GetSoilType()
+        {
+            return this.soilType;
+        }
+
+
+        internal void Measure()
+        {
+            //lastCommand = "TR";
+            PogoSend("0");
+            PogoSend("0");
+            PogoSend("0");
+            PogoSend("T");
+            PogoSend("R");
+            PogoSend("\r");
+            PogoSend("\n");
+            System.Threading.Thread.Sleep(2000);
+
+            lastCommand = "T0";
+            PogoSend("0");
+            PogoSend("0");
+            PogoSend("0");
+            PogoSend("T");
+            PogoSend("0");
+            PogoSend("\r");
+            PogoSend("\n");
+        }
+    }
+}
